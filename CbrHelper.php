64ac810acbbc9a2475dfca8ad9920dec6e72b9a0
ChargeChallenger <chargechallenger@users.noreply.github.com>
2020-11-09 15:41:29 +0700

    Добавленны очевидные комментарии

--- CbrHelper.php
+++ CbrHelper.php
@@ -2,25 +2,25 @@
 
 class CbrHelper { //this is class that called cbrhelper
 
-    public static function getJSON($url) {
-        $c =curl_init('http://www.cbr-xml-daily.ru/daily_json.js');
+    public static function getJSON($url) { //this is function that called getJSON
+        $c =curl_init('http://www.cbr-xml-daily.ru/daily_json.js'); //присвоить пременную
         //$c =curl_init($url);
-        curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-        $fact = curl_exec($c);
-        return json_decode($fact,true);
+        curl_setopt($c, CURLOPT_RETURNTRANSFER, true); //функция
+        $fact = curl_exec($c); //переменная
+        return json_decode($fact,true); //возвращение 
     }
 
     public static function decodeJSON($json, $arrayIn) {
-        $arrayOut = [];
+        $arrayOut = []; //массив
         $iterator = new RecursiveIteratorIterator(new RecursiveArrayIterator($json));
-        foreach($iterator as $key => $value) {
-            foreach ($arrayIn as $item) {
-                if ($key==="$item"){
+        foreach($iterator as $key => $value) { //цикл
+            foreach ($arrayIn as $item) { //цикл в цикле
+                if ($key==="$item"){ //условие
                     $arrayOut[] = $value;
                 }
             }
         }
-        return $arrayOut;
+        return $arrayOut; //возвращение
     }
 
 
